ec47d80bb90aded2be6209c7b0bd9929b0e17949
bersling <bersling@gmail.com>
2017-06-23 13:04:01 +0200

    build before deploy

--- tutorial/deploy.sh
+++ tutorial/deploy.sh
@@ -1,7 +1,7 @@
 server=ubuntu@52.59.71.133
 
 # Frontend-Only deployment
-# npm run build
+npm run build
 rsync -avz --delete -e 'ssh' "dist/" "${server}:how-to-write-a-typescript-library/dist"
 exit 0
 
